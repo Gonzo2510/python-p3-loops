d3edf0050c396aaa197afe5874c5eb360a4c2dfc
Jacob Gonzales <jagonz@live.com>
2023-10-14 11:35:01 -0500

    move x+=1 to end

--- lib/looping.py
+++ lib/looping.py
@@ -19,16 +19,12 @@ def fizzbuzz():
     while x <= 100:
         if x % 3 == 0 and x % 5 == 0:
             print('FizzBuzz')
-            x+=1
         elif x % 3 == 0:
             print('Fizz')
-            x+=1
         elif x % 5 == 0:
             print('Buzz')
-            x+=1
         else: 
             print(x)
-            x+=1
+        x+=1
 
-
-
+fizzbuzz()
